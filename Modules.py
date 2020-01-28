0022e9b29ac19294820eaf7948f9bdd434a4c97a
h19carmo <55438972+h19carmo@users.noreply.github.com>
2020-01-28 15:50:00 +0100

    u

--- Modules.py
+++ Modules.py
@@ -19,7 +19,7 @@ def meny():
         
         val = input("Välj ett av följande alternativ: ")
         if val =="1":
-            print(filen())
+            filen()
         elif val =="2":
             print('Du har valt att läsa in Originalfilen')
         elif val =="3":
@@ -32,31 +32,5 @@ def meny():
             break
 
 def filen():
-    import csv 
     
-    lines = []
-    try:
-        with open('personer.csv') as csv_file:
-            for line in csv_file:
-                lines.append(line)
-            return lines
-    except FileNotFoundError as ferr:
-        print(ferr) 
 
-"""
-    f = open('personer.csv', 'r')
-    
-    reader = csv.reader(f)
-
-    personer = []
-
-    for row in reader:
-        try:
-            people.append([row[0], row[1], row[2], row[3]])
-        except:
-            pass
-
-    print('Här är originalfilen personer.csv')
-    for item in personer:
-        print(item)
-"""
